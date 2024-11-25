1d6da77792d1722794258408a26ae94b2a2d99ff
Utkarshagar <nischalagarwal264@gmail.com>
2024-11-26 01:27:27 +0530

    update2.o

--- BACKEND/Routes/appointmentRoutes.js
+++ BACKEND/Routes/appointmentRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { bookAppointment, getAppointments } = require('../Controllers/AppointmentController');
-const { verifyToken } = require('../Middlewares/authMiddleware');
+const ensureAuthenticated = require('../Middlewares/Auth');
 
 const router = express.Router();
+console.log('bookAppointment:',ensureAuthenticated, bookAppointment);
+console.log('getAppointments:',ensureAuthenticated, getAppointments);
 
 // Define routes
-router.post('/book', verifyToken, bookAppointment);
-router.get('/', verifyToken, getAppointments);
+router.post('/book', bookAppointment);
+router.get('/', getAppointments);
 
 module.exports = router;
--- Frontend/login.js
+++ Frontend/login.js
@@ -1,5 +1,5 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();  // Prevent form submission
+    event.preventDefault(); // Prevent form submission
 
     // Get form values
     const role = document.getElementById('role').value;
@@ -30,9 +30,18 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     .then(response => response.json())
     .then(data => {
         if (data.success) {
-            // If login is successful, redirect user to a dashboard or home page
+            // If login is successful, store user and token in localStorage
+            localStorage.setItem('loggedInUser', data.name); // Save user data
+            localStorage.setItem('authToken', data.jwtToken); // Save token
+            localStorage.setItem('Role', role);
+            // Notify user of success and redirect
             alert('Login successful');
-            window.location.href = data.redirectUrl || 'dashboard.html'; // Redirect to the dashboard or specified page
+            if(role=="Admin")
+            window.location.href ='Dashboard/Admin.html'; // Redirect to dashboard or specified page
+            else if(role=="Doctor")
+                window.location.href ='Dashboard/doctor.html';
+            else 
+            window.location.href ='Dashboard/patient.html';
         } else {
             // Show error message if login fails
             alert(data.message || 'Login failed. Please check your credentials.');
