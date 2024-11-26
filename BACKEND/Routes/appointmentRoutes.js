cbc580b77aeaab4e50ba8e689dd3f6486784a7b2
pulkitupadhay <pulkit.upadhyay_cs22@gla.ac.in>
2024-11-26 09:02:24 +0530

    updated

--- BACKEND/Routes/appointmentRoutes.js
+++ BACKEND/Routes/appointmentRoutes.js
@@ -5,15 +5,11 @@ const ensureAuthenticated = require('../Middlewares/Auth');
 const router = express.Router();
 console.log('bookAppointment:',ensureAuthenticated, bookAppointment);
 console.log('getAppointments:',ensureAuthenticated, getAppointments);
-
 // Define routes
-<<<<<<< HEAD
 router.post('/some-path', someMiddleware, someControllerFunction);
 
 router.get('/', verifyToken, getAppointments);
-=======
 router.post('/book', bookAppointment);
 router.get('/', getAppointments);
->>>>>>> 1d6da77792d1722794258408a26ae94b2a2d99ff
 
 module.exports = router;
