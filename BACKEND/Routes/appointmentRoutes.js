0bcd00ff940c2e60892842e3096f0ef52de8b371
pulkitupadhay <pulkit.upadhyay_cs22@gla.ac.in>
2024-11-26 15:12:25 +0530

    updated

--- BACKEND/Routes/appointmentRoutes.js
+++ BACKEND/Routes/appointmentRoutes.js
@@ -7,13 +7,10 @@ console.log('bookAppointment:',ensureAuthenticated, bookAppointment);
 console.log('getAppointments:',ensureAuthenticated, getAppointments);
 
 // Define routes
-<<<<<<< HEAD
 router.post('/some-path', someMiddleware, someControllerFunction);
 
 router.get('/', verifyToken, getAppointments);
-=======
 router.post('/book', bookAppointment);
 router.get('/', getAppointments);
->>>>>>> 1d6da77792d1722794258408a26ae94b2a2d99ff
 
 module.exports = router;
