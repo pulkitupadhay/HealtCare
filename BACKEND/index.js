566ae9058401b5638465346449e9ba855e0c7bfd
Utkarshagar <nischalagarwal264@gmail.com>
2024-11-27 11:13:34 +0530

    update changes

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,20 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./Routes/AuthRouter'); // Existing auth routes
-const appointmentRoutes = require('./Routes/appointmentRoutes'); // New appointment routes
+const authRoutes = require('./Routes/AuthRouter'); 
+const appointmentRoutes = require('./Routes/appointmentRoutes'); 
 require('dotenv').config();
-require('./Models/db'); // Ensure database connection
+require('./Models/db');
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 });
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-app.use('/auth', authRoutes); // Existing auth routes
-app.use('/appointments', appointmentRoutes); // New appointment routes
+app.use('/auth', authRoutes); 
+app.use('/appointments', appointmentRoutes); 
